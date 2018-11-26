a3327b2588e9de9904b31fcce679221048b96b2a
Mark Stachowski <markstachowski@me.com>
2018-11-26 15:13:18 -0600

    adding methods

--- FileIOTest.java
+++ FileIOTest.java
@@ -1,31 +1,17 @@
 package contacts;
 
-import util.Input;
-
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Scanner;
+
 
 import static contacts.FileIO.contactMain;
 import static contacts.FileIO.readFileAsString;
 
 public class FileIOTest {
     public static void main(String[] args) {
-        Input in = new Input(new Scanner(System.in));
 
-        String diectory = "/Users/markstachowski/Projects/Codeup/IntelliJProjects" +
-                "/codeup-java-exercises/src/contacts";
-        String filename = "contacts.txt";
-
-        Path contactsPath = Paths.get(diectory, filename);
-
-        try {
-            System.out.println(readFileAsString(contactsPath));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-//        contactMain();
+        contactMain();
 
 
     }
