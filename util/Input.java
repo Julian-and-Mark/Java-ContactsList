4c4cca2d11fc3d6b7f31bbdcd5b9331443378f63
Mark Stachowski <markstachowski@me.com>
2018-11-26 14:45:38 -0600

    adding util class

---
+++ util/Input.java
@@ -0,0 +1,99 @@
+package util;
+
+import java.util.Scanner;
+
+public class Input {
+    private Scanner scanner;
+
+    public Input(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public String getString() {
+        return scanner.nextLine();
+    }
+
+    public boolean yesNo() {
+        String userInput = getString();
+        return (userInput.equalsIgnoreCase("y") || userInput.equalsIgnoreCase("yes"));
+    }
+
+    public int getInt() {
+        int output;
+        try {
+            output = Integer.valueOf(getString());
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid integer. Please try again.");
+            return getInt();
+        }
+        return output;
+    }
+
+    public int getInt(int min, int max) {
+        int userInt = getInt();
+        if (userInt >= min && userInt <= max) {
+            return userInt;
+        } else {
+            System.out.println("Number out of range!");
+            return getInt(min, max);
+        }
+    }
+
+    public double getDouble() {
+        double output;
+        try {
+            output = Double.valueOf(getString());
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid integer. Please try again.");
+            return getDouble();
+        }
+        return output;
+    }
+
+    public double getDouble(int min, int max) {
+        double userDouble = getDouble();
+        if (userDouble >= min && userDouble <= max) {
+            return userDouble;
+        } else {
+            System.out.println("Number out of range!");
+            return getDouble(min, max);
+        }
+    }
+
+    public int getBinary() {
+        int output;
+        try {
+            output = Integer.valueOf(getString(), 2);
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid binary number. Please try again.");
+            return getBinary();
+        }
+        return output;
+    }
+
+    public int getHex() {
+        int output;
+        try {
+            output = Integer.valueOf(getString(), 16);
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid hexadecimal number. Please try again.");
+            return getHex();
+        }
+        return output;
+    }
+// Extra inputs
+    public int countVowels(String input) {
+        return input.replaceAll("[^aeiouAEIOU]", "").length();
+    }
+
+
+
+    public static void main(String[] args) {
+        Input in = new Input(new Scanner(System.in));
+        System.out.println("Please enter a number: ");
+        int userInt = in.getHex();
+        System.out.println(userInt);
+    }
+
+
+}
