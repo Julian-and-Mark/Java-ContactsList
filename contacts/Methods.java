a1f2b9da71d951dcfcfa3fae7bc1a76cb292e087
Mark Stachowski <markstachowski@me.com>
2018-11-27 11:33:24 -0600

    fixed encapsulation in FileIOTest

--- contacts/Methods.java
+++ contacts/Methods.java
@@ -16,7 +16,7 @@ public class Methods {
         try {
             System.out.println("Name | Phone number");
             System.out.println("-------------------");
-            List<String> contacts = Files.readAllLines(Paths.get("contacts/contacts.txt"));
+            List<String> contacts = Files.readAllLines(Paths.get("src/contacts/contacts.txt"));
 
             for (String line : contacts) {
                 System.out.println(line);
@@ -30,7 +30,7 @@ public class Methods {
 
     public static void addContact() {
         Input scan = new Input(new Scanner(System.in));
-        Path p = Paths.get("contacts/contacts.txt");
+        Path p = Paths.get("src/contacts/contacts.txt");
 
         String nameAdd;
         String numberAdd;
@@ -56,7 +56,7 @@ public class Methods {
         System.out.println("Enter the contact you are looking for: ");
         input2 = scan1.getString();
         try {
-            List<String> lines = Files.readAllLines(Paths.get("contacts/contacts.txt"));
+            List<String> lines = Files.readAllLines(Paths.get("src/contacts/contacts.txt"));
             for (String line : lines) {
                 if (line.contains(input2)) {
                     System.out.println(line);
@@ -69,7 +69,7 @@ public class Methods {
     }
 
     public static void deleteContact() {
-        Path p = Paths.get("contacts", "contacts.txt");
+        Path p = Paths.get("src/contacts", "contacts.txt");
         Input scan = new Input(new Scanner(System.in));
         System.out.println("Which contact would you like to delete?");
         String contactToDelete = scan.getString();
@@ -85,6 +85,42 @@ public class Methods {
         } catch (IOException e) {
             System.out.println("Exception");
             e.printStackTrace();
+        }
+    }
+
+    public void start() {
+        while (true) {
+            System.out.println(
+                    "1. View contacts.\n" +
+                            "2. Add a new contact.\n" +
+                            "3. Search a contact by name.\n" +
+                            "4. Delete an existing contact.\n" +
+                            "5. Exit.\n" +
+                            "Enter an option (1, 2, 3, 4 or 5)");
+
+            Input input = new Input(new Scanner(System.in));
+
+            int choice = input.getInt();
+            switch (choice) {
+                case 1:
+                    Methods show = new Methods();
+                    show.showAll();
+                    break;
+                case 2:
+                    Methods add = new Methods();
+                    add.addContact();
+                    break;
+                case 3:
+                    Methods search = new Methods();
+                    search.searchContacts();
+                    break;
+                case 4:
+                    Methods delete = new Methods();
+                    delete.deleteContact();
+                    break;
+                case 5:
+                    return;
+            }
         }
     }
 }
